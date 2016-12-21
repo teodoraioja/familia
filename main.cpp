d3a4ed4c4e38ceee8a340ed529ffceca1fbae80b
teo <teo@yahoo.com>
2016-12-21 15:36:21 +0200

    project get started

---
+++ main.cpp
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: teo
+ *
+ * Created on December 20, 2016, 1:19 PM
+ */
+
+using namespace std;
+#include <iostream>
+#include <cstdlib>
+
+/*
+ * 
+ */
+int main() {
+	
+	cout << "Hello World! \n";
+	return 0;
+}
+
