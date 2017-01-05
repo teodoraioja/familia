8ed88d2f939862044c7d5ba105494fe4ff27195b
teo <teo@yahoo.com>
2017-01-05 10:08:41 +0200

    Starting project

---
+++ mainwindow.cpp
@@ -0,0 +1,26 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include<QFileDialog>
+#include<QMessageBox>
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_openFile_clicked()
+{
+QString filename= QFileDialog::getOpenFileName(
+            this,
+            tr("Open File"),
+            "C://",
+            "Text File(*.txt)"
+            );
+QMessageBox::information(this,tr("File Name"), filename);
+}
